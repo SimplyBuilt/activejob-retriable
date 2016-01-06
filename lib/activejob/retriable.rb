b76c06b90ca1b034f74fe07946d909c024555bec
Michael Coyne <mikeycgto@gmail.com>
2016-01-06 12:49:22 -0500

    Remove Rails 4 monkey patch (#2)

--- lib/activejob/retriable.rb
+++ lib/activejob/retriable.rb
@@ -5,17 +5,3 @@ module ActiveJob
 
   autoload :Retriable
 end
-
-# ActiveJob deserialization monkey patch
-# NOTE this will be removed once Rails 5 is released
-unless ActiveJob::Base.instance_methods.include?(:deserialize)
-  module ActiveJob
-    class Base
-      def self.deserialize(job_data)
-        job               = super(job_data)
-        job.retry_attempt = job_data['retry_attempt'] if job.respond_to?(:retry_attempt=)
-        job
-      end
-    end
-  end
-end
