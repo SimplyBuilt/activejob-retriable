26d1421666b0f60d0d6865485e7822da7c26899c
Cameron Craig <sentrathis96@gmail.com>
2016-07-21 12:15:11 -0700

    Simplify test_helper

--- lib/active_job/retriable/test_helper.rb
+++ lib/active_job/retriable/test_helper.rb
@@ -3,33 +3,10 @@ require 'active_job/retriable/test_adapater'
 module ActiveJob
   module Retriable
     module TestHelper
-      extend ActiveSupport::Concern
+      include ActiveJob::TestHelper
 
-      included do
-        include ActiveJob::TestHelper
-
-        def before_setup
-          super
-
-          # Initialize our custom adapter
-          retriable_adapter = ActiveJob::Retriable::TestAdapter.new
-
-          # This is from active_job/test_helper
-          ActiveJob::Base.queue_adapter = retriable_adapter
-          ActiveJob::Base.subclasses.each do |klass|
-            # only override explicitly set adapters, a quirk of `class_attribute`
-            if klass.singleton_class.public_instance_methods(false).include?(:_queue_adapter)
-              klass.queue_adapter = retriable_adapter
-            end
-          end
-
-          clear_enqueued_jobs
-          clear_performed_jobs
-        end
-
-        def after_teardown
-          super
-        end
+      def queue_adapter_for_test
+        ActiveJob::Retriable::TestAdapter.new
       end
     end
   end
